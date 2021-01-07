a9715be08e2c37bb5b2a486b8f41722876a94433
DESKTOP-GFR9A7S\shreyshah <shreyshah.612@gmail.com>
2021-01-07 12:59:51 +1000

    Was comfortable able to access the nested dictionary

--- test.py
+++ test.py
@@ -7,16 +7,13 @@ base_url = "http://127.0.0.1:5000/"
 response = requests.get(base_url + "racing")
 
 resp = response.json()
-print(type(resp))
-print(resp)
-df = pd.DataFrame(resp.items())
-print(type(df))
-print(df)
 
 
-# def get_all_values(nested_dictionary):
-#     for key, value in nested_dictionary.items():
-#         if (type(value) is dict):
-#             get_all_values(value)
-#         else:
-#             print(key, ":", value)
+def get_all_values(nested_dictionary):
+    for key, value in nested_dictionary.items():
+        if (type(value) is dict):
+            get_all_values(value)
+        else:
+            print(key, ":", value)
+
+get_all_values(resp)
